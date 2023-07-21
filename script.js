7f08a66af51d4d8b86bb2c700b52e053d7b58f7a
Vikram <vikram.jaisingh123@gmail.com>
2023-07-22 00:00:39 +0200

    links in new tab

---
+++ script.js
@@ -0,0 +1,11 @@
+function Devpost() {
+  window.open("https://devpost.com/vikram-jaisingh123", "_blank");
+}
+
+function Youtube() {
+  window.open("https://www.youtube.com/channel/UCSfWnQvsojYXIofrKCUH7Ww");
+}
+
+function Resume() {
+  window.open("uploads/Jaisingh, Vikramaditya Resume.pdf");
+}
