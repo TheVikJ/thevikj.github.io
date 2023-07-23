624eba28808e4945853b4371b5d20ad69b3d4d39
Vikram <vikram.jaisingh123@gmail.com>
2023-07-23 04:23:24 +0200

    cursor+ projects+

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-function Devpost() {
-  window.open("https://devpost.com/vikram-jaisingh123", "_blank");
-}
-
 function Youtube() {
   window.open("https://www.youtube.com/channel/UCSfWnQvsojYXIofrKCUH7Ww");
 }
 
 function Resume() {
   window.open("uploads/Jaisingh, Vikramaditya Resume.pdf");
+}
+
+function Devpost() {
+  window.open("https://devpost.com/vikram-jaisingh123", "_blank");
+}
+
+function Marvelous() {
+  window.open("https://devpost.com/software/marvelous-ym2h5a");
+}
+
+function Suave() {
+  window.open("https://devpost.com/software/s-u-a-v-e");
+}
+
+function Paladin() {
+  window.open("https://devpost.com/software/playwrightpaladin");
+}
+
+function Ocarina() {
+  window.open("https://devpost.com/software/ocarina");
 }
