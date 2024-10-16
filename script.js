5ce2e807d9bf933911deebc28a05c30931607bce
Vikram <vikram.jaisingh123@gmail.com>
2024-10-16 14:18:51 -0400

    redesign home page

--- script.js
+++ script.js
@@ -10,22 +10,6 @@ function Devpost() {
   window.open("https://devpost.com/vikram-jaisingh123", "_blank");
 }
 
-function Marvelous() {
-  window.open("https://devpost.com/software/marvelous-ym2h5a");
-}
-
-function Suave() {
-  window.open("https://devpost.com/software/s-u-a-v-e");
-}
-
-function Paladin() {
-  window.open("https://devpost.com/software/playwrightpaladin");
-}
-
-function Ocarina() {
-  window.open("https://devpost.com/software/ocarina");
-}
-
 function ViksBlue() {
   window.open("https://viks.blue");
 }
